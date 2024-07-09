20c8b404ae46a91397c1368534ed68ce216e1df2
alexandre-niess <alexandrefersoa@gmail.com>
2024-07-09 15:23:35 -0300

    Criação das rotas protegidas (rotas que só podem ser acessadas depois do login)

---
+++ src/ProtectedRoute.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+const ProtectedRoute = ({ children }) => {
+  const auth = getAuth();
+  const [user, setUser] = React.useState(null);
+  const [loading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, [auth]);
+
+  if (loading) {
+    return <div>Loading...</div>; // ou um componente de carregamento
+  }
+
+  if (!user) {
+    return <Navigate to="/login" />;
+  }
+
+  return children;
+};
+
+export default ProtectedRoute;

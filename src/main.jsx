a072b94b3d5bbe9074aeca479717b4a993f533c3
alexandre-niess <alexandrefersoa@gmail.com>
2024-07-09 15:23:55 -0300

    Ajustes no tema

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PerfilEmp from "../screens/PerfilEmp.jsx";
 import { createRoot } from "react-dom";
 import { CadPrato } from "../screens/CadPrato.jsx";
+import { Login } from "../screens/Login.jsx";
+import { CadAdmin } from "../screens/CadAdmin.jsx";
+import ProtectedRoute from "./ProtectedRoute"; // Importando o componente de rota protegida
+import CadRestaurante from "../screens/CadRestaurante/CadRestaurante.jsx";
+import Admin from "../screens/Admin.jsx";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -14,7 +19,18 @@ createRoot(document.getElementById("root")).render(
         <Route path="/AppRestaurante" element={<App />}>
           <Route index element={<Home />} />
           <Route path="perfil-da-loja" element={<PerfilEmp />} />
-          <Route path="admin" element={<CadPrato />} />
+          <Route path="login" element={<Login />} />
+          <Route path="cadastro-admin" element={<CadAdmin />} />
+          <Route path="cadastro-restaurante" element={<CadRestaurante />} />
+          <Route
+            path="admin"
+            element={
+              <ProtectedRoute>
+                <Admin />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="*" element={<h1>Not Found</h1>} />
         </Route>
       </Routes>
     </BrowserRouter>

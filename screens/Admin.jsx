b6d1a49da135c618253802e1f818a0245c8b97bc
alexandre-niess <alexandrefersoa@gmail.com>
2024-07-09 21:12:19 -0300

    Adição dos botões para as telas de cadastro do restaurante e para a de cadastro de produtos

--- screens/Admin.jsx
+++ screens/Admin.jsx
@@ -7,12 +7,55 @@ import Typography from "@mui/material/Typography";
 import Header from "../components/Header";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
+import { Link } from "react-router-dom";
 
 const cardData = [
-  { id: 1, title: "Categorias Cadastradas", data: ["Categoria 1", "Categoria 2", "Categoria 3", "Categoria 4", "Categoria 5", "Categoria 6", "Categoria 7"] },
-  { id: 2, title: "Formas de Pagamento", data: ["Cartão de Crédito", "Cartão de Débito", "Dinheiro", "Pix", "Vale Refeição", "Vale Alimentação", "Transferência Bancária"] },
-  { id: 3, title: "Horarios de Atendimento", data: ["Seg: 10:00 - 22:00", "Ter: 10:00 - 22:00", "Qua: 10:00 - 22:00", "Qui: 10:00 - 22:00", "Sex: 10:00 - 22:00", "Sáb: 10:00 - 22:00", "Dom: 10:00 - 22:00"] },
-  { id: 4, title: "Endereço", data: ["Avenida Dom José Gaspar, 500 - Coração Eucarístico, Belo Horizonte - MG, 30535-901"] }
+  {
+    id: 1,
+    title: "Categorias Cadastradas",
+    data: [
+      "Categoria 1",
+      "Categoria 2",
+      "Categoria 3",
+      "Categoria 4",
+      "Categoria 5",
+      "Categoria 6",
+      "Categoria 7",
+    ],
+  },
+  {
+    id: 2,
+    title: "Formas de Pagamento",
+    data: [
+      "Cartão de Crédito",
+      "Cartão de Débito",
+      "Dinheiro",
+      "Pix",
+      "Vale Refeição",
+      "Vale Alimentação",
+      "Transferência Bancária",
+    ],
+  },
+  {
+    id: 3,
+    title: "Horarios de Atendimento",
+    data: [
+      "Seg: 10:00 - 22:00",
+      "Ter: 10:00 - 22:00",
+      "Qua: 10:00 - 22:00",
+      "Qui: 10:00 - 22:00",
+      "Sex: 10:00 - 22:00",
+      "Sáb: 10:00 - 22:00",
+      "Dom: 10:00 - 22:00",
+    ],
+  },
+  {
+    id: 4,
+    title: "Endereço",
+    data: [
+      "Avenida Dom José Gaspar, 500 - Coração Eucarístico, Belo Horizonte - MG, 30535-901",
+    ],
+  },
 ];
 
 export function Admin() {
@@ -21,10 +64,10 @@ export function Admin() {
       <Header />
       <Box
         sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          marginTop: '100px',
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          marginTop: "100px",
         }}
       >
         <Grid container spacing={2} justifyContent="center">
@@ -33,18 +76,22 @@ export function Admin() {
               <Card
                 sx={{
                   padding: 2,
-                  backgroundColor: "#f9f9f9", // cor de fundo clara
-                  boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)", // sombra sutil
-                  borderRadius: "8px", // bordas arredondadas
-                  textAlign: 'center',
-                  display: 'flex',
-                  flexDirection: 'column',
-                  justifyContent: 'space-between',
-                  minHeight: '300px', // altura mínima para todos os cards
+                  backgroundColor: "background.secondary",
+                  boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
+                  borderRadius: "8px",
+                  textAlign: "center",
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "space-between",
+                  minHeight: "300px",
+                  border: "1px solid #e0e0e0",
                 }}
               >
                 <CardContent sx={{ flexGrow: 1 }}>
-                  <Typography component="h1" sx={{ fontSize: "18px", fontWeight: "600" }}>
+                  <Typography
+                    component="h1"
+                    sx={{ fontSize: "18px", fontWeight: "600" }}
+                  >
                     {card.title}
                   </Typography>
                   <Box
@@ -54,33 +101,27 @@ export function Admin() {
                       height: "100%",
                       marginTop: 2,
                       marginBottom: 2,
-                      justifyContent: 'center',
-                      minHeight: '150px', // altura mínima para a área cinza
-                      alignItems: 'center',
-                      backgroundColor: "#e0e0e0", // cor de fundo para a área cinza
+                      justifyContent: "center",
+                      minHeight: "150px",
+                      alignItems: "center",
+
                       padding: 2,
-                      borderRadius: '4px'
+                      borderRadius: "4px",
                     }}
                   >
                     {card.data.map((item, index) => (
-                      <Typography key={index} component="p" sx={{ fontSize: "14px" }}>
+                      <Typography
+                        key={index}
+                        component="p"
+                        sx={{ fontSize: "14px" }}
+                      >
                         {item}
                       </Typography>
                     ))}
                   </Box>
                 </CardContent>
                 <CardActions>
-                  <Button
-                    fullWidth
-                    variant="contained"
-                    sx={{
-                      backgroundColor: "#ff3d00", // cor de fundo vermelha para o botão
-                      color: "white",
-                      '&:hover': {
-                        backgroundColor: "#e53935", // cor de fundo ao passar o mouse
-                      }
-                    }}
-                  >
+                  <Button fullWidth variant="contained">
                     EDITAR
                   </Button>
                 </CardActions>
@@ -89,6 +130,29 @@ export function Admin() {
           ))}
         </Grid>
       </Box>
+      <Link to="/AppRestaurante/cad-prato">
+        <Button
+          variant="contained"
+          sx={{
+            position: "fixed",
+            bottom: "20px",
+            right: "20px",
+          }}
+        >
+          Adicionar produto
+        </Button>
+      </Link>
+      <Link to="/AppRestaurante/cadastro-restaurante">
+        <Button
+          variant="contained"
+          sx={{
+            bottom: "20px",
+            right: "20px",
+          }}
+        >
+          Cadastrar restaurante
+        </Button>
+      </Link>
     </>
   );
 }
